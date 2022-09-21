ba79f0293f00d700c16d2e030a6ba47be27ab6b6
Jordan Booker <jordanlbooker2020@gmail.com>
2022-09-21 14:18:50 -0500

    Store the flick in an object

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const app = {
+    max: 0,
     init: function(formSelector) {
+        this.max = 0
+
          document
             .querySelector(formSelector)
-            .addEventListener('submit', this.handleSubmit)
+            .addEventListener('submit', ev => {
+                ev.preventDefault()
+                this.handleSubmit(ev)  
+            })
     },
 
     handleSubmit: function(ev) {
-        ev.preventDefault()
         const f = ev.target
-        console.log(f.flickName.value)
+        const flick = {
+            id: ++this.max,
+            name: f.flickName.value,
+
+        }
+       
+        console.log(flick)
+        f.reset
     },
 }
 
